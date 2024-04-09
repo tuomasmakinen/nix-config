4d169bf55cc07e42ed3b8bf3600c73e754d72528
t4sm5n <28858039+t4sm5n@users.noreply.github.com>
2024-04-09 13:58:42 +0300

    Add direnv and neovim

--- home/t4sm5n/satonaka.nix
+++ home/t4sm5n/satonaka.nix
@@ -1,6 +1,19 @@
-{ outputs, lib, ... }: {
+{ outputs, lib, pkgs, ... }: {
   imports = [ outputs.darwinTrampolines ./global ];
   home.homeDirectory = lib.mkForce "/Users/t4sm5n";
   programs.firefox.enable = lib.mkForce false;
   programs.kitty.font.size = lib.mkForce 16;
+
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+  };
 }
